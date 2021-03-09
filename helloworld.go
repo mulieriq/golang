05fe3257546eb09f7d58278deaa66b9117ef783f
EricRootLee <erickiokom@gmail.com>
2021-03-09 22:41:32 +0300

    coditions

--- helloworld.go
+++ helloworld.go
@@ -5,25 +5,29 @@ import (
 )
 
 func main() {
-	muli:="34ggjitgaio"
+	muli := "34ggjitgaio"
 	fmt.Print("Enter a number: ")
-	fmt.Println("1+1 =",14%5,muli[1:2])
-	fmt.Println("erix"+"afaf")
-	fmt.Println("erifsmg","fhaf")
+	fmt.Println("1+1 =", 14%5, muli[1:2])
+	fmt.Println("erix" + "afaf")
+	fmt.Println("erifsmg", "fhaf")
 	fmt.Println(!(false || true))
 	var x int = 6
 	fmt.Println(x)
-	v:=8
+	v := 8
 	println(v)
 	var name string
 	fmt.Println("Please enter name")
 	fmt.Scan(&name)
-	fmt.Println("your name is",name)
+	fmt.Println("your name is", name)
 
-	i:=10
-	for i >=0{
-		fmt.Println(i)
-		i--
+	i := 10
+	y := true
+	for y == true {
+		for i >= 0 {
+			fmt.Println(i)
+			i--
+		}
+		y = false
 	}
 
 }
