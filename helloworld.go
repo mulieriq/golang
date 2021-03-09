d4bb842fa0f853959f1069416b5d061cb40c97b2
EricRootLee <erickiokom@gmail.com>
2021-03-09 21:42:09 +0300

    Implicit & explicit variables

--- helloworld.go
+++ helloworld.go
@@ -10,8 +10,10 @@ func main() {
 	fmt.Println("1+1 =",14%5,muli[1:2])
 	fmt.Println("erix"+"afaf")
 	fmt.Println("erifsmg","fhaf")
-
 	fmt.Println(!(false || true))
-
+	var x int = 6
+	fmt.Println(x)
+	v:=8
+	print(v)
 
 }
