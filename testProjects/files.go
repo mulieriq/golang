ad223a8783a304d4f4294290d82925652d59a183
EricRootLee <erickiokom@gmail.com>
2021-03-18 17:07:38 +0300

    Os create an write

--- testProjects/files.go
+++ testProjects/files.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func main() {
-	f, _ := os.Create("test.txt")
-	f.Write([]byte("Hello world"))
+	f, _ := os.OpenFile("test.txt", os.O_APPEND|os.O_WRONLY , 0644)
+	 	//os.Create("test.txt")
+	 	newline := "New dat added"
+//	f.Write([]byte("Hello world"))
+_,err := fmt.Fprint(f,newline)
 
+fmt.Println(err)
 }
