b69c6bc424332eeebafae39bc74ac78b179ae552
EricRootLee <erickiokom@gmail.com>
2021-03-20 23:45:35 +0300

    wprking on swithch cases

--- oreilly/introducingGo.go
+++ oreilly/introducingGo.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var (
 		a = 2
-		b =3
-		c=5
+		b = 3
+		c = 5
 	)
-	fmt.Println("Numbers",a,b,c)
+	fmt.Println("Numbers", a, b, c)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("NUm", i)
+		switch i {
+		case 2:
+			fmt.Println(i)
+		}
+		}
 
 }
