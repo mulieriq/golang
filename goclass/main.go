de394221278f9b489c18a7226a5f491e547db18b
EricRootLee <erickiokom@gmail.com>
2021-06-01 15:23:55 +0300

    Tibits

--- goclass/main.go
+++ goclass/main.go
@@ -5,61 +5,58 @@ import (
 	"os"
 )
 
-type Person struct{
-	Name string
-	Power int
+type Person struct {
+	Name   string
+	Power  int
 	Father *Person
 }
 
-func (s *Person) super(power int){
-	 s.Power += power
+func (s *Person) super(power int) {
+	s.Power += power
 }
 
-func exp(){
+func exp() {
 	goku := &Person{
-	   Name:"Gustavo",
-	   Power : 3,
-	   Father: &Person{
-		   Name:"Walter",
-		   Power:6,
-		   Father:nil,
-		   },
-	   }
-	   goku.super(2)
+		Name:  "Gustavo",
+		Power: 3,
+		Father: &Person{
+			Name:   "Walter",
+			Power:  6,
+			Father: nil,
+		},
+	}
+	goku.super(2)
 
-	   fmt.Println(goku.Power)
-           goku.slice()
-   }
+	fmt.Println(goku.Power)
+	goku.slice()
+}
 
-func main(){
+func main() {
 	fmt.Println("Hello")
 
 	if len(os.Args) != 2 {
 		os.Exit(1)
 	}
-        fmt.Println("Its Over man",os.Args[0],os.Args[1])
-        name := power(os.Args[1])
-        fmt.Println("MULTY",name)
+	fmt.Println("Its Over man", os.Args[0], os.Args[1])
+	name := power(os.Args[1])
+	fmt.Println("MULTY", name)
 	exp()
-	
+
 }
 
-func power(args string)(string){
-	 return args
+func power(args string) string {
+	return args
 }
 
-func(p * Person) slice(){
+func (p *Person) slice() {
 
- scores := []int{1,2,3,4,5,6,7,8,9}
+	scores := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
- for _,value :=range scores{
-   for i := 1 ; i<=9 ; i++{
-    fmt.Println(value,"*" ,i,"=", i*value)
+	for _, value := range scores {
+		for i := 1; i <= 9; i++ {
+			fmt.Println(value, "*", i, "=", i*value)
 
-
-
-
-  }
- }
+		}
+	}
 
 }
