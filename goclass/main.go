44ef38d77f3fd4381348a7ac6e5965881a109fa1
EricRootLee <erickiokom@gmail.com>
2021-06-01 15:35:42 +0300

    Errors:

--- goclass/main.go
+++ goclass/main.go
@@ -3,33 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
-type Person struct {
-	Name   string
-	Power  int
-	Father *Person
-}
-
-func (s *Person) super(power int) {
-	s.Power += power
-}
-
-func exp() {
-	goku := &Person{
-		Name:  "Gustavo",
-		Power: 3,
-		Father: &Person{
-			Name:   "Walter",
-			Power:  6,
-			Father: nil,
-		},
-	}
-	goku.super(2)
-
-	fmt.Println(goku.Power)
-	goku.slice()
-}
+//type Person struct {
+//	Name   string
+//	Power  int
+//	Father *Person
+//}
+//
+//func (s *Person) super(power int) {
+//	s.Power += power
+//}
+//
+//func exp() {
+//	goku := &Person{
+//		Name:  "Gustavo",
+//		Power: 3,
+//		Father: &Person{
+//			Name:   "Walter",
+//			Power:  6,
+//			Father: nil,
+//		},
+//	}
+//	goku.super(2)
+//
+//	fmt.Println(goku.Power)
+//	goku.slice()
+//}
 
 func main() {
 	fmt.Println("Hello")
@@ -37,26 +38,33 @@ func main() {
 	if len(os.Args) != 2 {
 		os.Exit(1)
 	}
-	fmt.Println("Its Over man", os.Args[0], os.Args[1])
-	name := power(os.Args[1])
-	fmt.Println("MULTY", name)
-	exp()
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		println("non number")
 
-}
-
-func power(args string) string {
-	return args
-}
-
-func (p *Person) slice() {
-
-	scores := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	for _, value := range scores {
-		for i := 1; i <= 9; i++ {
-			fmt.Println(value, "*", i, "=", i*value)
-
-		}
+	} else {
+		print(n)
 	}
+	//fmt.Println("Its Over man", os.Args[0], os.Args[1])
+	//name := power(os.Args[1])
+	//fmt.Println("MULTY", name)
+	//exp()
 
 }
+
+//func power(args string) string {
+//	return args
+//}
+//
+//func (p *Person) slice() {
+//
+//	scores := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+//
+//	for _, value := range scores {
+//		for i := 1; i <= 9; i++ {
+//			fmt.Println(value, "*", i, "=", i*value)
+//
+//		}
+//	}
+//
+//}
